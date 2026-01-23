a087c447780a2d3328fbb1f961aec88e2803612d
Arman Beisembin <arman.beisembin@gmail.com>
2026-01-23 10:11:18 +0500

    Архитектура: передача зависимостей

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"go/adv-demo/configs"
+	"go/adv-demo/internal/auth"
+
 	"net/http"
 )
 
 func main() {
+	conf := configs.LoadConfig()
 	router := http.NewServeMux()
-	NewHelloHandler(router)
+	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
+		Config: conf,
+	})
 
 	server := http.Server{
 		Addr:    ":8081",
